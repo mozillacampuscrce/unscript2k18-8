1734d4a910991dde327aec2f57297cd7cd4dadd6
Aashish Nehete <ashnehete@gmail.com>
2018-03-24 16:48:47 +0530

    Util+Rest

---
+++ android/Chehra/student/src/main/java/com/codeblooded/chehra/student/Constants.java
@@ -0,0 +1,34 @@
+package com.codeblooded.chehra.student;
+
+/**
+ * Created by Aashish Nehete on 24-Mar-18.
+ */
+
+public class Constants {
+    public static final String APP_PREFS = "app_preferences";
+    public static final String USER_PREFS = "user_preferences";
+
+    public static final String LOGGED_IN = "logged_in";
+    public static final String FIRST_START = "first_start";
+
+    public static final String TOKEN = "token";
+    public static final String ID = "student_id";
+    public static final String USERNAME = "username";
+    public static final String EMAIL = "email";
+    public static final String FIRST_NAME = "first_name";
+    public static final String LAST_NAME = "last_name";
+    public static final String UID = "uid";
+    public static final String DEPT_ID = "dept_id";
+    public static final String YEAR = "year";
+    public static final String ACADEMIC_YR = "academic_yr";
+    public static final String COURSE_ID = "course_id";
+    public static final String VIDEO = "video";
+    public static final String IS_VIDEO_ADDED = "is_video_added";
+
+    public static final String DEBUG_SERVER_ENABLED = "debug_server_enabled";
+    public static final String DEBUG_SERVER_URL = "debug_server_url";
+
+    public static final String COURSE = "course";
+    public static final String IS_ENROLLED = "is_enrolled";
+    public static final String START_TIME = "start_time";
+}
