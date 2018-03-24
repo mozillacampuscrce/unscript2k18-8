83ee06752080ad4b9ee826965ee44b550eac5b2d
TejasBhitle <bhitletejas@gmail.com>
2018-03-24 21:04:50 +0530

    lectures-student added

--- backend/src/apiApp/views.py
+++ backend/src/apiApp/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Lecture, Student, Course
+
+
+# Create your views here.
+@csrf_exempt
+def index(request):
+    return JsonResponse({'text': 'hello This is index page',
+                         'status': '200 OK'}
+                        )
+
+
+def markAttendance(lect_id, student_id, has_attended):
+
+    lecture = Lecture.objects.filter(lect_id=lect_id).first()
+    student = Student.objects.filter(student_id=student_id).first()
+
+    if has_attended:
+        lecture.students.add(student)
+    else:
+        lecture.students.remove(student)
+
+
+def isStudentEnrolledInCourse(student_id, course):
+    # student = Student.objects.filter(student_id=student_id)
+    course = Course.objects.filter(course_id=course.course_id).first()
+    queryset = course.students.filter(student_id=student_id)
+    print(queryset)
+    return queryset.count() == 1
