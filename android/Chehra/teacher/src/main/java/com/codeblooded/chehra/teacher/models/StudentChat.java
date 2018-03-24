f1fd2edb44a6b90407a61170569d2609c910f64e
Aashish Nehete <ashnehete@gmail.com>
2018-03-24 23:51:09 +0530

    StudentChatList added

--- android/Chehra/teacher/src/main/java/com/codeblooded/chehra/teacher/models/StudentChat.java
+++ android/Chehra/teacher/src/main/java/com/codeblooded/chehra/teacher/models/StudentChat.java
@@ -5,4 +5,39 @@ package com.codeblooded.chehra.teacher.models;
  */
 
 public class StudentChat {
+
+    private String studentID;
+    private String firebaseID;
+
+    private String name;
+
+    public StudentChat(String studentID, String firebaseID,String name) {
+        this.studentID = studentID;
+        this.firebaseID = firebaseID;
+        this.name = name;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getFirebaseID() {
+        return firebaseID;
+    }
+
+    public void setFirebaseID(String firebaseID) {
+        this.firebaseID = firebaseID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
