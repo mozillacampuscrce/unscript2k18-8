f6657ff2df3c8bdd54854f900b9f9cc6f4173a94
Aashish Nehete <ashnehete@gmail.com>
2018-03-25 02:54:45 +0530

    Parents->teacher list

---
+++ android/Chehra/student/src/main/java/com/codeblooded/chehra/student/models/TeacherChat.java
@@ -0,0 +1,66 @@
+package com.codeblooded.chehra.student.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Aashish Nehete on 24-Mar-18.
+ */
+
+public class TeacherChat implements Parcelable {
+    public static final Creator<StudentChat> CREATOR = new Creator<StudentChat>() {
+        @Override
+        public StudentChat createFromParcel(Parcel source) {
+            return new StudentChat(source);
+        }
+
+        @Override
+        public StudentChat[] newArray(int size) {
+            return new StudentChat[size];
+        }
+    };
+    private String studentID;
+    private String name;
+
+    public TeacherChat() {
+
+    }
+
+    public TeacherChat(String studentID, String name) {
+
+        this.studentID = studentID;
+        this.name = name;
+    }
+
+    protected TeacherChat(Parcel in) {
+        this.studentID = in.readString();
+        this.name = in.readString();
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.studentID);
+        dest.writeString(this.name);
+    }
+}
---
+++ android/Chehra/student/src/main/java/com/codeblooded/chehra/student/ui/adapters/TeacherChatListAdapter.java
@@ -0,0 +1,76 @@
+package com.codeblooded.chehra.student.ui.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.codeblooded.chehra.teacher.Constants;
+import com.codeblooded.chehra.teacher.R;
+import com.codeblooded.chehra.teacher.models.StudentChat;
+import com.codeblooded.chehra.teacher.ui.activities.ChatActivity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Aashish Nehete on 24-Mar-18.
+ */
+
+public class TeacherChatListAdapter extends RecyclerView.Adapter<StudentChatListAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<StudentChat> studentChats;
+
+    public TeacherChatListAdapter(Context context, ArrayList<StudentChat> studentChats) {
+        this.context = context;
+        this.studentChats = studentChats;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.list_item_student_chat, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final StudentChat chat = studentChats.get(position);
+        holder.nameTextView.setText(chat.getName());
+
+        holder.parentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, ChatActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putParcelable(Constants.CHAT, chat);
+                intent.putExtras(bundle);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return studentChats.size();
+    }
+
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView nameTextView;
+        LinearLayout parentView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            parentView = itemView.findViewById(R.id.parentView);
+            nameTextView = itemView.findViewById(R.id.nameTextView);
+        }
+    }
+
+
+}
