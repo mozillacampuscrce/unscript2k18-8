99e4af1bb760b5544638ee79a44c34867a9a18a0
Kaustubh Toraskar <kaustubhtoraskar97@gmail.com>
2018-03-25 08:25:18 +0530

    circularListView added

---
+++ android/Chehra/teacher/src/main/java/com/codeblooded/chehra/teacher/ui/activities/CircularActivity.java
@@ -0,0 +1,12 @@
+package com.codeblooded.chehra.teacher.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CircularActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
